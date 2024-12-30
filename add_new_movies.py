e7db335ca9d22bd0448b69af806ab06e9d603555
Anurag Rawat <anurag.rawat0201@gmail.com>
2024-12-30 13:29:16 +0000

    implement function add_new_movies

---
+++ add_new_movies.py
@@ -0,0 +1,8 @@
+def add_new_movies():
+    movie_catalog = []
+    all_genres = set()
+    movie_title = input("Enter the movie title: ")
+    movie_genre = input("Enter the movie genre: ")
+    movie_catalog.append(movie_title)
+    all_genres.add(movie_genre)
+
